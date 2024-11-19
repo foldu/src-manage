b81f2560766c06351f076189f436f0dd29f7284a
foldu <foldu@protonmail.com>
2024-11-19 19:31:18 +0100

    sync: always run filter
    Makes no difference

--- src/main.rs
+++ src/main.rs
@@ -107,12 +107,12 @@ fn sync(config: &Config, host: &str) -> Result<(), eyre::Error> {
     target.push(&path);
     target.push("/");
 
-    let mut cmd = Command::new("rsync");
-    if fs::exists(".gitignore").context("Failed statting .gitignore")? {
-        cmd.arg("--filter=:- .gitignore");
-    }
-
-    cmd.arg("-azvP").arg("./").arg(target).run()?;
+    Command::new("rsync")
+        .arg("--filter=:- .gitignore")
+        .arg("-azvP")
+        .arg("./")
+        .arg(target)
+        .run()?;
 
     Ok(())
 }
